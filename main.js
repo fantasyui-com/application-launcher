88885dd36650b8ac32baae8e5fc3b3409f21a514
Dr. Meow <captain@fantasyui.com>
2019-07-21 20:31:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences:{nodeIntegration: true}})
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
